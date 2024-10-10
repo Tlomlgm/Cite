fd174e0ea713d9a28f4776d581edccd628ffc6f7
Tloml <142570455+Tlomlgm@users.noreply.github.com>
2024-10-10 18:54:15 +0800

    更新 91aw.js

--- 91aw.js
+++ 91aw.js
@@ -33,7 +33,7 @@ if (matchedUrl && matchedUrl.length > 0) {
                 $.log(`设置新的m3u8 URL: ${matchedUrl}`);
 
                 const senPlayerUrl = `SenPlayer://x-callback-url/play?url=${encodeURIComponent(matchedUrl)}`;
-                const mediaUrl = "https://raw.githubusercontent.com/Yu9191/-/main/dingdangmao.jpg";
+                const mediaUrl = "https://raw.githubusercontent.com/Tlomlgm/Icon/main/messy/SenPlayer.png";
 
                 $.log(`SenPlayer URL: ${senPlayerUrl}`);
                 $.log(`Media URL: ${mediaUrl}`);
