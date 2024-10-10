b0c06841ec02c1ed710d2bc84c3ee38c845db2bf
Tloml <142570455+Tlomlgm@users.noreply.github.com>
2024-10-10 20:17:41 +0800

    更新 91Stay.js

--- 91Stay.js
+++ 91Stay.js
@@ -39,7 +39,7 @@ if (matchedUrl && matchedUrl.length > 0) {
                 $.log(`Media URL: ${mediaUrl}`);
 
                 // 确保正确使用 $.msg 方法
-                $.msg("获取成功", "点击打开SenPlayer播放器", "已安装请忽略", {
+                $.msg("获取成功", "点击通知使用Stay下载", "☼☀︎☼☀︎☼☀︎", {
                     "open-url": senPlayerUrl,
                     "media-url": mediaUrl,
                 });
