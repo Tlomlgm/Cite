9597191c5170252520838651d389c08674202c1e
Tloml <142570455+Tlomlgm@users.noreply.github.com>
2024-10-10 18:41:47 +0800

    更新 91aw.js

--- 91aw.js
+++ 91aw.js
@@ -38,7 +38,7 @@ if (matchedUrl && matchedUrl.length > 0) {
                 $.log(`SenPlayer URL: ${senPlayerUrl}`);
                 $.log(`Media URL: ${mediaUrl}`);
 
-                // 确保正确使用 $.msg 方法，添加缺失的逗号
+                // 确保正确使用 $.msg 方法
                 $.msg("获取成功", "点击通知使用SenPlayer播放", {
                     "open-url": senPlayerUrl,
                     "media-url": mediaUrl,
