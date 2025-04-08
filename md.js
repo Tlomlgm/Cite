3ae286a25b82b6270b03697ec17fc003bc53deb1
Tloml <142570455+Tlomlgm@users.noreply.github.com>
2025-04-09 00:00:15 +0800

    更新 md.js

--- md.js
+++ md.js
@@ -22,11 +22,11 @@ const $ = new Env("麻豆社区");
 // 视频无法播放联系作者TG更新
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJwdWJsaWMiLCJleHAiOjE3NDY2MzU1NDMsImlzc3VlciI6ImNvbS5idXR0ZXJmbHkiLCJzdWIiOiJhc2lnbiIsInVzZXJJZCI6MTcwNjI3NjkxfQ.DUQdJOKVJP_C4PRV1eccbQ1fAXwDbs1d1KVrUntSIt0";
 // 获取选择的播放器
-const selectedPlayer = $argument.Player || "Safari";  // 从 $argument.Player 获取选择的播放器，默认 Safari
+const playerScheme = $argument.Player || "Safari";  // 从 $argument.Player 获取选择的播放器，默认 Safari
 $.log(`选择的播放器: ${selectedPlayer}`);
 
 // 播放器 scheme 对应的 URL 和图标
-const playerScheme = {
+const argument.Player = {
     "SenPlayer": "SenPlayer://x-callback-url/play?url=",
     "Infuse": "infuse://x-callback-url/play?url=",
     "PotPlayer": "potplayer://url=",
