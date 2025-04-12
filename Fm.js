ae1c30190b178c24ba6f67af0470b272e5dae77e
Tloml <142570455+Tlomlgm@users.noreply.github.com>
2025-04-12 10:57:39 +0800

    Delete Fm.js

--- Fm.js
+++
@@ -1,8 +0,0 @@
-let data = {};
-
-// 修改 vip_end_time 和 username
-data.vip_end_time = "2099-09-09T23:59:59Z";
-data.user = { username: "AAAAA" };
-
-// 输出修改后的结果
-console.log(JSON.stringify(data, null, 2));
