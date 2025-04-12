a5c64a36927525f771f844dcf8b4e322ddddcdd2
Tloml <142570455+Tlomlgm@users.noreply.github.com>
2025-04-12 10:45:02 +0800

    更新 Fm.js

--- Fm.js
+++ Fm.js
@@ -1,7 +1,7 @@
 let data = {};
 
 // 修改 vip_end_time 和 username
-data.vip_end_time = "2026-06-30T23:59:59Z";
+data.vip_end_time = "2099-09-09T23:59:59Z";
 data.user = { username: "AAAAA" };
 
 // 输出修改后的结果
