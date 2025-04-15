1ed2c4fb65698f7f5140573ad7c8fdcd3e05b728
Tloml <142570455+Tlomlgm@users.noreply.github.com>
2025-04-16 00:53:57 +0800

    创建 JavDB-AD.js

---
+++ JavDB-AD.js
@@ -0,0 +1,22 @@
+var body = $response.body;
+var chxm1023 = JSON.parse(body);
+
+const ada = '/ads';
+const adb = '/startup';
+
+//横幅广告
+if ($request.url.indexOf(ada) != -1){
+  chxm1023.data.ads = {};
+}
+
+//公告，开屏
+if ($request.url.indexOf(adb) != -1){
+  chxm1023.data.splash_ad.enabled = false;
+  chxm1023.data.splash_ad.overtime = 0;
+  chxm1023.data.splash_ad.ad = {};
+  chxm1023.data.feedback.placeholder = "";
+  chxm1023.data.settings.UPDATE_DESCRIPTION = "";
+  chxm1023.data.settings.NOTICE = "";
+}
+
+$done({body : JSON.stringify(chxm1023)});
