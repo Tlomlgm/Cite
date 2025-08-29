e14af1f2d7ddb7db20ebbb77bc0a52a23f5ca63a
Tloml <142570455+Tlomlgm@users.noreply.github.com>
2025-08-29 15:13:18 +0800

    更新 JavDB.js

--- JavDB.js
+++ JavDB.js
@@ -3,7 +3,7 @@ let header = $request.headers;
 
 if (url.includes("/api/v1/movies/") && url.includes("/play?")) {
   header.authorization =
-    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6OTc1NDMwLCJ1c2VybmFtZSI6IndlaWd1YW5naHQifQ.lyfGvtZcz0SjiKNx-k9Aoe_UgcMyxwG4Xqq3lzvbIao";
+    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MzU4NDg3NywidXNlcm5hbWUiOiJjaHhtMTAyNCJ9.RI3cy6hTiFd7NgzDxN8UJwWlCQEJtGGxqRryWW8jr-w";
   $done({ headers: header });
 } else {
   $done({});
